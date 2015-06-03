52abbc0dc6a20b5358563c7e06d63f5838ddb7f9
Aldur <adrianodl@hotmail.it>
2015-06-03 12:13:43 +0200

    improve tests

--- t/start_spoolgore.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-"$(dirname "$(readlink -f "$0")")"/../spoolgore \
-    -smtpaddr 127.0.0.1:8025 \
-    /tmp/spoolgore_t \
-    -freq 1  \
-
--- t/test_spoolgore.py
+++ t/test_spoolgore.py
@@ -1,91 +1,148 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-import smtpd
 import time
 import asyncore
 import threading
-import nose
 import unittest
+import os
+import signal
+import json
+import subprocess
+import shutil
+import smtpd
 
 import os.path
 
 from email.mime.text import MIMEText
-from nose.tools import assert_equals
+from nose.tools import assert_equals, assert_is_not_none, nottest
 
+SPOOLGORE_EXECUTABLE = "./spoolgore"
 
 SPOOL_DIRECTORY = "/tmp/spoolgore_t"
 JSON_STATS = os.path.join(SPOOL_DIRECTORY, ".spoolgore.js")
 
 LOCAL_ADDRESS = ("127.0.0.1", 8025)
-REMOTE_ADDRESS = ("127.0.0.1", 8026)
+LOCAL_ADDRESS_S = ':'.join([str(s) for s in LOCAL_ADDRESS])
 
 DEFAULT_SENDER = "foo@bar.com"
 DEFAULT_RECEIVER = "jhon@doe.com"
 DEFAULT_SUBJECT = "Look, an email!"
 
+EVENT_TIMEOUT = 10
+
+
+def setup_module():
+    try:
+        os.mkdir(SPOOL_DIRECTORY)
+    except OSError:
+        pass
+
+
+def teardown_module():
+    shutil.rmtree(SPOOL_DIRECTORY, ignore_errors=True)
+
 
 class MockSMTPServer(smtpd.SMTPServer, object):
 
-    def __init__(self, localhost, remotehost):
-        super(MockSMTPServer, self).__init__(localhost, remotehost)
-        self.mails = dict()
+    def __init__(self, localhost, fake_errors=False):
+        super(MockSMTPServer, self).__init__(
+            localhost,
+            None)
+        self.fake_errors = fake_errors
+        self.mails = list()
+        self.event = threading.Event()
 
     def process_message(self, peer, mailfrom, rcpttos, data):
-        self.mails[mailfrom] = data
+        self.mails.append(data)
+        self.event.set()
+
+        if self.fake_errors:
+            return '550 No such user here'
+        return None
 
     def clear(self):
         self.mails = dict()
+        self.event.clear()
 
     @property
     def count(self):
         return len(self.mails)
 
+    @property
+    def last(self):
+        if self.mails:
+            return self.mails[-1]
+        return None
 
-def spoool_mail(content, sender, receiver, subject=None):
+
+def spoool_mail(content, sender=DEFAULT_SENDER, receiver=DEFAULT_RECEIVER, subject=DEFAULT_SUBJECT):
     """
     Put mail in the spooler directory.
-    Sender and receiver must be mail Address.
     """
     msg = MIMEText(content)
     msg['From'] = sender
     msg['To'] = receiver
+    msg['Subject'] = subject
 
-    if subject:
-        msg['Subject'] = subject
+    path = os.path.join(SPOOL_DIRECTORY, str(time.time()))
 
-    with open(os.path.join(SPOOL_DIRECTORY, str(time.time())), 'w') as mail_f:
+    with open(path, "w") as mail_f:
         mail_f.write(msg.as_string())
+
+    return path
+
+
+def start_spoolgore(arguments):
+    arguments = [SPOOLGORE_EXECUTABLE] + arguments
+    return subprocess.Popen(arguments, shell=False)
 
 
 class TestSpoolgore(unittest.TestSuite, object):
 
-    def setup(self):
-        "Setup test fixtures here."
-        self.server = MockSMTPServer(LOCAL_ADDRESS, REMOTE_ADDRESS)
+    @classmethod
+    def setup_class(cls):
+        cls.spoolgore = start_spoolgore(["-smtpaddr", LOCAL_ADDRESS_S, "-attempts", "5", "-freq", "1", SPOOL_DIRECTORY])
 
-        self.thread = threading.Thread(
-            target=asyncore.loop, name="Asyncore Loop", kwargs={'timeout': 1}
+        cls.server = MockSMTPServer(LOCAL_ADDRESS)
+
+        cls.thread = threading.Thread(
+            target=asyncore.loop, kwargs={'timeout': 1}
         )
-        self.thread.daemon = True
-        self.thread.start()
+        cls.thread.daemon = True
+        cls.thread.start()
+
+    @classmethod
+    def teardown_class(cls):
+        cls.spoolgore.terminate()
+
+        cls.server.close()
+        cls.thread.join()
 
     def teardown(self):
-        "Teardown test fixtures here"
-        pass
-        # self.server.close()
-        # self.thread.join()
+        self.server.clear()
 
     def test_base_mail(self):
-        spoool_mail(
-            "Base email check", DEFAULT_SENDER, DEFAULT_RECEIVER, DEFAULT_SUBJECT
-        )
-        time.sleep(10)
+        spoool_mail("Base email check")
+
+        self.spoolgore.send_signal(signal.SIGURG)
+        self.server.event.wait()
+
         assert_equals(self.server.count, 1)
+        mail = self.server.last
+        assert_is_not_none(mail)
 
+    @nottest
+    def test_attempts(self):
+        self.server.fake_errors = True
 
-def main():
-    nose.main()
+        path = spoool_mail("Base email with attempts")
+        for i in range(5):
+            self.server.event.wait()
+            self.server.event.clear()
 
-if __name__ == '__main__':
-    main()
+        with open(JSON_STATS) as json_stats:
+            j = json.loads(json_stats.read())
+            print(j[path])
+
+        self.server.fake_errors = False
